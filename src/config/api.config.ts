0c3e5442e54010f54935e703e6efe7162dc2607e
Dejan Ristic <dejan.ristic@factoryww.com>
2025-10-02 02:19:17 +0200

    caching

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,9 +1,13 @@
-import axios from "axios";
+import { setup } from "axios-cache-adapter";
 
-const api = axios.create({
+const api = setup({
   baseURL: "https://www.thesportsdb.com/api/v1/json/3/",
   headers: {
     "Content-Type": "application/json",
+  },
+  cache: {
+    maxAge: 15 * 60 * 1000,
+    exclude: { query: false },
   },
 });
 
